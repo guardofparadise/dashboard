93c377a0c1cebf489a68fd3db3948131a00d2041
Vlad <pokidin@ram.com.ua>
2019-06-25 14:27:00 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AnnouncementCard from './components/Card/AnnouncementCard';
+import AuthForm from './components/AuthForm';
+import './styles/reduction.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <AnnouncementCard />
+			 <AuthForm />
       </header>
     </div>
   );
