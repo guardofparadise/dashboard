f61e0b06ff13df1f5271dcecf2efcd31b3d1ead9
Vlad <pokidin@ram.com.ua>
2019-07-01 19:27:55 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import AnnouncementCard from './components/Card/AnnouncementCard';
+import AvatarCard from './components/Card/AvatarCard';
 import AuthForm from './components/AuthForm';
+import { NumberWidget, IconWidget } from './components/Widget';
 import './styles/reduction.scss';
 
 function App() {
@@ -9,6 +11,9 @@ function App() {
       <header className="App-header">
        <AnnouncementCard />
 			 <AuthForm />
+			 <IconWidget />
+			 <NumberWidget />
+			 <AvatarCard />
       </header>
     </div>
   );
